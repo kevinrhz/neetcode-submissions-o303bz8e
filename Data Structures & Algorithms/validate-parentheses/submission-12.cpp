e73f766ce1c20ba949540c4cceb5d067cee06aa7
Kevin <92818810+kevinrhz@users.noreply.github.com>
2026-06-12 20:59:03 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::vector<char> stack;
+        stack.reserve(s.size());
+
+        for (const char c : s) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push_back(c);
+                    break;
+                
+                case ')':
+                    if (stack.empty() || stack.back() != '(') return false;
+                    stack.pop_back();
+                    break;
+                case '}':
+                    if (stack.empty() || stack.back() != '{') return false;
+                    stack.pop_back();
+                    break;
+                case ']':
+                    if (stack.empty() || stack.back() != '[') return false;
+                    stack.pop_back();
+                    break;
+            }
+        }
+        return stack.empty();
+    }
+};
