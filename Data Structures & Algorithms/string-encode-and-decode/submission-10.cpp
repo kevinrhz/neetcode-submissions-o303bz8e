0aaafb22a4ae5471850438da4d2952dcb59fc883
Kevin <92818810+kevinrhz@users.noreply.github.com>
2026-08-30 21:34:59 -0700

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    string encode(const vector<string>& strs) {
+        std::string res;
+        for (const std::string& s : strs) {
+            res.append(std::to_string(s.size())+ "#" + s);
+        }
+        return res;
+    }
+
+    vector<string> decode(const string& s) {
+        std::vector<std::string> res;
+
+        int i = 0;
+        while (i < s.size()) {
+            int delim_pos = s.find('#', i);
+            int len = std::stoi(s.substr(i, delim_pos - i));
+            
+            std::string word = s.substr(delim_pos + 1, len);
+            res.push_back(std::move(word));
+            
+            i = delim_pos + 1 + len;
+        }
+        
+        return res;
+    }
+};
